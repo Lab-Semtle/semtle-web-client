41d7659b33a86bff6641cf3c7bc4614f30d44ce1
kimminseo <rlaa8644@gmail.com>
2024-05-24 21:44:00 +0900

    Add: 자유게시판 페이징기능 구현

--- src/pages/Board/Boardlist.js
+++ src/pages/Board/Boardlist.js
@@ -3,135 +3,117 @@ import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Navbarboot from '../../components/Header/Navbarboot';
 import { Link } from 'react-router-dom';
-import {ApiURL} from '../../ApiURL/ApiURL';
+import { ApiURL } from '../../ApiURL/ApiURL';
 import './CommonTable.css';
 
-/*const postList = [
-    {
-      "no": 1,
-      "title": "첫번째 게시글입니다.",
-      "content": "첫번째 게시글 내용입니다.",
-      "createDate": "2020-10-25",
-      "readCount": 6
-    },
-    {
-      "no": 2,
-      "title": "두번째 게시글입니다.",
-      "content": "두번째 게시글 내용입니다.",
-      "createDate": "2020-10-25",
-      "readCount": 5
-    },
-    {
-      "no": 3,
-      "title": "세번째 게시글입니다.",
-      "content": "세번째 게시글 내용입니다.",
-      "createDate": "2020-10-25",
-      "readCount": 1
-    },
-    {
-      "no": 4,
-      "title": "네번째 게시글입니다.",
-      "content": "네번째 게시글 내용입니다.",
-      "createDate": "2020-10-25",
-      "readCount": 2
-    },
-    {
-      "no": 5,
-      "title": "다섯번째 게시글입니다.",
-      "content": "다섯번째 게시글 내용입니다.",
-      "createDate": "2020-10-25",
-      "readCount": 4
-    },
-  ];*/
+import PaginationBasic from '../../components/Header/PaginationBasic';
 
 const CommonTableRow = ({ children }) => {
-    return (
-  
-      <tr className="common-table-row" >
-        {
-          children
-        }
-        
-      </tr>
+  return (
 
-    )
-  }
-  const CommonTableColumn = ({ children }) => {
-    return (
-      <td className="common-table-column">
-        {
-          children
-        }
-      </td>
-    )
-  }
-const CommonTable = (props) => {
-    const { headersName, children } = props;
+    <tr className="common-table-row" >
+      {
+        children
+      }
 
-    return (
-        <table className="common-table">
-          <thead>
-            <tr>
-              {
-                headersName.map((item, index) => {
-                  return (
-                    <th className="common-table-header-column" key={index}>{ item }</th>
-                  )
-                })
-              }
-            </tr>
-          </thead>
-          <tbody>
-          
-            {
-              children
-            }
-          
-          </tbody>
-        </table>
-      )
+    </tr>
+
+  )
 }
+const CommonTableColumn = ({ children }) => {
+  return (
+    <td className="common-table-column">
+      {
+        children
+      }
+    </td>
+  )
+}
+const CommonTable = (props) => {
+  const { headersName, children } = props;
+
+  return (
+    <table className="common-table">
+      <thead>
+        <tr>
+          {
+            headersName.map((item, index) => {
+              return (
+                <th className="common-table-header-column" key={index}>{item}</th>
+              )
+            })
+          }
+        </tr>
+      </thead>
+      <tbody>
+
+        {
+          children
+        }
+
+      </tbody>
+    </table>
+  )
+}
+
 const Boardlist = props => {
 
-    const [boardList, setBoardList] = useState([]);
-    const getBoardList = async () => {
-        const resp = await (await axios.get(`${ApiURL.Boardlist_get}`)).data; // 2) 게시글 목록 데이터에 할당
-        setBoardList(resp.data); // 3) boardList 변수에 할당
-        console.log(Array.isArray(resp.data));
-        const pngn = resp.pagination;
-        console.log(pngn);
-    }
-    
-    useEffect(() => {
-        getBoardList(); // 1) 게시글 목록 조회 함수 호출
-      }, []);
-    
-    //useEffect(() => {setBoardList(postList);}, [ ])
-    return (
-      <>
-      <Navbarboot></Navbarboot>
-        <CommonTable headersName={['글번호', '제목', '등록일', '조회수']}>
-        {
-        
-         
-        
-          boardList ? boardList.map((item, index) => {
-            return (
-              <CommonTableRow key={index} >
-                <CommonTableColumn>{ item.no }</CommonTableColumn>
-                <CommonTableColumn><Link to={`/Boardview/${index}`}>{ item.title }</Link></CommonTableColumn>
-                <CommonTableColumn>{ item.createDate }</CommonTableColumn>
-                <CommonTableColumn>{ item.readCount }</CommonTableColumn>
-              </CommonTableRow>
-
-            )
-          }) : ''
-        }
-        </CommonTable>
-        <button><Link to="/Boardcreate">게시물 쓰기</Link></button>
-      </>
-    )
+  const [boardList, setBoardList] = useState([]);
+  const getBoardList = async () => {
+    const resp = await (await axios.get(`${ApiURL.Boardlist_get}`)).data; // 2) 게시글 목록 데이터에 할당
+    setBoardList(resp.data); // 3) boardList 변수에 할당
+    setPosts(resp.data);
   }
+
+  useEffect(() => {
+    getBoardList(); // 1) 게시글 목록 조회 함수 호출
+    currentPosts(boardList);
+  }, []);
+
+
+
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);//현 페이지 인덱스
+  const [postsPerPage, setPostsPerPage] = useState(10);//
+
+
+  const indexOfLast = currentPage * postsPerPage;//게시글 인덱스 끝
+  const indexOfFirst = indexOfLast - postsPerPage;//게시글 인덱스 마지막
+
+  const currentPosts = (posts) => {
+    let currentPost = 0;
+    currentPost = posts.slice(indexOfFirst, indexOfLast);
+
+    return currentPost;
+  };
+
+
+  //useEffect(() => {setBoardList(postList);}, [ ])
+  return (
+    <>
+      <Navbarboot></Navbarboot>
+      <div className='boardlist-table'>
+        <CommonTable headersName={['글번호', '제목', '등록일', '조회수']}>
+          {
+            currentPosts(posts) ? currentPosts(posts).map((item, index) => {
+              return (
+                <CommonTableRow key={index} >
+                  <CommonTableColumn>{item.no}</CommonTableColumn>
+                  <CommonTableColumn><Link to={`/Boardview/${index}`}>{item.title}</Link></CommonTableColumn>
+                  <CommonTableColumn>{item.createDate}</CommonTableColumn>
+                  <CommonTableColumn>{item.readCount}</CommonTableColumn>
+                </CommonTableRow>
+              )
+            }) : ''
+          }
+        </CommonTable>
+      </div>
+      <div className='boardcreate-button'><button><Link to="/Boardcreate">게시물 쓰기</Link></button></div>
+      <div className="pagination-basic"><PaginationBasic postsPerPage={postsPerPage} totalPosts={posts.length} paginate={setCurrentPage} currentPage={currentPage}></PaginationBasic></div>
+    </>
+  )
+}
 
 export default Boardlist;
 
--- src/pages/Board/Boardview.js
+++ src/pages/Board/Boardview.js
@@ -1,37 +1,38 @@
-import React, { useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import Navbarboot from "../../components/Header/Navbarboot";
 import axios from "axios";
 import { useParams } from 'react-router-dom';
-import {ApiURL} from '../../ApiURL/ApiURL';
-
-function Boardview(props){
+import { ApiURL } from '../../ApiURL/ApiURL';
+import './Boardview.css';
+function Boardview(props) {
     const { idx } = useParams(); // /Board/view/:idx와 동일한 변수명으로 데이터를 꺼낼 수 있습니다.
     const [loading, setLoading] = useState(true);
     const [board, setBoard] = useState({});
     const getBoard = async () => {
-        const resp = await (await axios.get(`${ApiURL.Boardview_get}`)).data;
+        const resp = await (await axios.get(`${ApiURL.Boardview_get}`)).data;//고정주소
+        //const resp = await (await axios.get(`${ApiURL.Boardview_get}/${idx}`)).data; 주소 달라짐
         setBoard(resp.data);
-        console.log(resp);
+        console.log(board);
         setLoading(false);
-      };
-      useEffect(() => {
+    };
+    useEffect(() => {
         getBoard();
-      }, []);
-      
-    return(<>
+    }, []);
+    return (<>
         <div>
             <Navbarboot></Navbarboot>
         </div>
-        <div>
-            {console.log(typeof board)}
-            {board.id}
-        </div>
-        <div>
-            아이디 입력날짜 수정 삭제버튼
-        </div>
+        <div className="board-view">
+            <div className="view-title">
+                <h2>{board.title}</h2>
+            </div>
+            <div className="view-menu">
+                <span>{board.username}</span><span>{board.createDate}</span><button>수정</button><button>삭제</button>
+            </div>
 
-        <div>본문</div>
-        </>
+            <div className="view-content">{board.content}</div>
+        </div>
+    </>
     );
 }
 
