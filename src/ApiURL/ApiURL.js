d51e898ab50869c23db226943583590acc22c119
kimminseo <rlaa8644@gmail.com>
2024-05-31 20:01:00 +0900

    리액트 17.0.2 다운그레이드 add: ToastEditor 기능 추가

--- src/ApiURL/ApiURL.js
+++ src/ApiURL/ApiURL.js
@@ -1,6 +1,6 @@
 const ApiURL={
     Boardlist_get:`https://gist.githubusercontent.com/minseozzing/976893f1a782e503607a12db93184ef8/raw/03d37c45236eb2774791fff15f621dc41ddfd42a/gsdfgergewxx.json`,
-    Boardview_get:`https://gist.githubusercontent.com/minseozzing/2036f94525929d9ac74b508dd036b263/raw/ade5029c4cf609926652ac33be0da611cd2f690f/gistfiwefwef.json`,
+    Boardview_get:`https://gist.githubusercontent.com/minseozzing/2036f94525929d9ac74b508dd036b263/raw/e7ff32b38655643b3ad3c4909f93b9da5cf73413/gistfiwefwef.json`,
     Boardcreate_post:`https://reqres.in/api/users/2`
 }
 export{ApiURL}
