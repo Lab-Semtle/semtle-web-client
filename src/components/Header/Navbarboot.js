0bf8e16443d8b9d88878742092f871bd9302d1e2
Bagsangbin <Bagsangbin01@gmail.com>
2024-09-06 21:36:05 +0900

    Refactor:대소문자 구분

--- src/components/Header/Navbarboot.js
+++ src/components/Header/Navbarboot.js
@@ -7,24 +7,30 @@ import { Link } from 'react-router-dom';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import LogoImg from "../../Logo.png"
+import { Apiurl } from '../../Apiurl/Apiurl';
 
 function Navbarboot() {
   const [accessToken, setAccessToken] = useState(null);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    axios.get('http://localhost:8000/api/v1/login/refresh')
+    axios.get(Apiurl.refresh_get)
       .then(response => {
-        setAccessToken(response.data.access_token);
+        console.log(response)
+        if(response.data.status === "success")
+          setAccessToken(true);
+        else
+          setAccessToken(false);
       })
       .catch(error => {
-        setAccessToken(null);
-      })
+        console.log(error);
+        setAccessToken(false);
+      })  
       .finally(() => {
         setLoading(false);
       });
   }, []);
-  
+
   if (loading) {
     return <div>Loading...</div>;
   }
@@ -50,25 +56,46 @@ function Navbarboot() {
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-            <Nav className="me-auto">
-              <Nav.Link as={Link} to="/Boardlist">자유게시판</Nav.Link>
-              <Nav.Link href="#pricing">교수님</Nav.Link>
-              <NavDropdown title="더보기" id="collapsible-nav-dropdown">
-                <NavDropdown.Item as={Link} to="/StudyBoardlist" >스터디게시판</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">
-                  족보게시판
-                </NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">00게시판</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">
-                  우리의 여정
-                </NavDropdown.Item>
-              </NavDropdown>
-            </Nav>
+          <Nav className="me-auto">
+            <Nav.Link as={Link} to="/Boardlist">게시판</Nav.Link>
+            <Nav.Link href="#pricing">교수님</Nav.Link>
+            <NavDropdown title="더보기" id="collapsible-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">sememem</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">sdfdsfsdf</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.3">sdfsdf</NavDropdown.Item>
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">sdfddfsd</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
           <Nav>
-            {accessToken !==null ? (
+            {accessToken ? (
               <>
-                <Nav.Link as={Link} to="/MyInfo">내 정보</Nav.Link>
+                <Nav.Link as={Link} to="/MyInfo">내정보</Nav.Link>
+                <Nav.Link
+                onClick={() => {
+                    axios.get(Apiurl.token_get)
+                         .then(response => {
+                            const accessToken = response.data.access_token;
+
+                               // 로그아웃 요청을 Authorization 헤더에 accessToken을 담아 보냅니다.
+                              return axios.get(Apiurl.logout_get, {
+                                  headers: {
+                                      Authorization: `Bearer ${accessToken}`
+                                  },
+                                  withCredentials: true
+                              });
+                          })
+                          .then(res => {
+                              // 로그아웃 성공 시 리다이렉트
+                              window.location.href = "/";
+                          })
+                          .catch(error => {
+                              console.error("Error during logout: ", error);
+                          });
+                  }}
+              >
+                  로그아웃
+              </Nav.Link>
               </>
             ) : (
               <Nav.Link as={Link} to="/Login" eventKey={2}>로그인</Nav.Link>
